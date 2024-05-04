3ac824fc86bb05f4fef72a8eb516aeca3decd106
fabianromeroromero <163800699+fabianromeroromero@users.noreply.github.com>
2024-05-03 19:55:22 -0500

    Initial commit

---
+++ app/src/main/java/com/example/whatsapp/RegistroActivity.java
@@ -0,0 +1,100 @@
+package com.example.whatsapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
+
+
+public class RegistroActivity extends AppCompatActivity {
+
+    Button btregistar;
+    EditText txtName, txtEmail, txtPassword;
+    FirebaseFirestore mFirestore;
+    FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_registro);
+
+        txtName= findViewById(R.id.txtName);
+        txtEmail= findViewById(R.id.txtEmail);
+        txtPassword= findViewById(R.id.txtPassword);
+        btregistar= findViewById(R.id.btregistar);
+
+        btregistar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String nameUser = txtName.getText().toString().trim();
+                String emailUser = txtEmail.getText().toString().trim();
+                String passUser = txtPassword.getText().toString().trim();
+
+                if (nameUser.isEmpty() && emailUser.isEmpty() && passUser.isEmpty()){
+                    Toast.makeText(RegistroActivity.this, "complete los datos", Toast.LENGTH_SHORT).show();
+                }else {
+                    registerUser(nameUser,emailUser,passUser);
+                }
+            }
+        });
+
+    }
+
+    private void registerUser(String nameUser, String emailUser, String passUser) {
+        mAuth.createUserWithEmailAndPassword(emailUser, passUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                String id = mAuth.getCurrentUser().getUid();
+                Map<String, Object> map = new HashMap<>();
+                map.put("id", id);
+                map.put("name", nameUser);
+                map.put("email", emailUser);
+                map.put("password", passUser);
+
+                mFirestore.collection("user").document(id).set(map).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        finish();
+                        startActivity(new Intent(RegistroActivity.this, MainActivity.class));
+                        Toast.makeText(RegistroActivity.this, "Usuario registrado con éxito", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(RegistroActivity.this, "Error al guardar", Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(RegistroActivity.this, "Error al registrar", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return false;
+    }
+}
